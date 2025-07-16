4919296e4d00ce5a8dccebdf56ba442175503deb
Prathmesh Thorat <prathmeshthorat406@gmail.com>
2025-07-16 17:02:01 +0530

    Add files via upload

---
+++ javastatement_&loops/For_loop/Reverse.java
@@ -0,0 +1,7 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String str = "Hello";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
+    }
+}
